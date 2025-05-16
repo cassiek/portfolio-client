85cf191958487a871180f334a734045199422343
cassiek <ckumpula123@gmail.com>
2025-05-16 15:37:40 -0400

    update name of resume file

--- src/pages/ResumePage/ResumePage.jsx
+++ src/pages/ResumePage/ResumePage.jsx
@@ -7,7 +7,7 @@ function ResumePage() {
         <main className="resume-page">
             <section className="resume-page__content">
                 <h3 className="resume-page__header">Resumé</h3>
-                <object data={API_URL+`/Cassie Kumpula Portfolio Resume.pdf`} type="application/pdf" className="resume-page__pdf"></object> 
+                <object data={API_URL+`/Cassie Kumpula Software Engineer.pdf`} type="application/pdf" className="resume-page__pdf"></object> 
             </section>
         </main>
     )
