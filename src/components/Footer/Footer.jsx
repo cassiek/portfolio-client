b912db27c2ffc0a22de5ae29776466aece128376
cassiek <ckumpula123@gmail.com>
2025-06-07 19:04:21 -0400

    import .svg icon as React component and update hover styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,20 @@
 import "./Footer.scss";
 import { Link } from "react-router-dom";
+import LinkedIn from "../../assets/icons/linkedin.png";
+import LinkedIn_drk from "../../assets/icons/linkedin-drkpurple.png";
+import Medium from "../../assets/icons/medium-brands-solid.svg?react";
+import GitHub from "../../assets/icons/github.png";
+import GitHub_drk from "../../assets/icons/github-drkpurple.png";
+import email from "../../assets/icons/email.png";
+import email_drk from "../../assets/icons/email-drkpurple.png";
 
 function Footer() {
     return (
         <footer className="footer">
-            <Link to="https://www.linkedin.com/in/cassie-k/" className="footer__linkedin-icon"></Link>
-            <Link to="https://github.com/cassiek" className="footer__github-icon"></Link>
-            <Link to="mailto:ckumpula123@gmail.com" className="footer__email-icon"></Link>
+            <Link to="https://www.linkedin.com/in/cassie-k/"></Link>
+            <Link to="https://medium.com/@cassiekumpula" ><Medium className="footer__icon" /></Link>
+            <Link to="https://github.com/cassiek"></Link>  
+            <Link to="mailto:ckumpula123@gmail.com"></Link>
             <p className="footer__bullet-point">•</p>
             <p className="footer__copyright">Cassandra Kumpula © 2025</p>
         </footer>
