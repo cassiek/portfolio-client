fb98705b60e856e4c163952925deea54126020a3
cassiek <ckumpula123@gmail.com>
2025-06-07 20:18:13 -0400

    replace footer icons with .svg icons, remove unused styling, update icon styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,17 @@
 import "./Footer.scss";
 import { Link } from "react-router-dom";
-import LinkedIn from "../../assets/icons/linkedin.png";
-import LinkedIn_drk from "../../assets/icons/linkedin-drkpurple.png";
-import Medium from "../../assets/icons/medium-brands-solid.svg?react";
-import GitHub from "../../assets/icons/github.png";
-import GitHub_drk from "../../assets/icons/github-drkpurple.png";
-import email from "../../assets/icons/email.png";
-import email_drk from "../../assets/icons/email-drkpurple.png";
+
+import LinkedIn from "../../assets/icons/linkedin-in-brands.svg?react";
+import GitHub from "../../assets/icons/github-brands.svg?react";
+import Email from "../../assets/icons/envelope-solid.svg?react";
+
 
 function Footer() {
     return (
         <footer className="footer">
-            <Link to="https://www.linkedin.com/in/cassie-k/"></Link>
-            <Link to="https://medium.com/@cassiekumpula" ><Medium className="footer__icon" /></Link>
-            <Link to="https://github.com/cassiek"></Link>  
-            <Link to="mailto:ckumpula123@gmail.com"></Link>
+            <Link to="https://www.linkedin.com/in/cassie-k/"><LinkedIn className="footer__icon" /></Link>
+            <Link to="https://github.com/cassiek"><GitHub className="footer__icon" /></Link>  
+            <Link to="mailto:ckumpula123@gmail.com"><Email className="footer__icon"/></Link>
             <p className="footer__bullet-point">•</p>
             <p className="footer__copyright">Cassandra Kumpula © 2025</p>
         </footer>
